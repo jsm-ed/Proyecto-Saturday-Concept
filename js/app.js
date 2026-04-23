bb8a31366dde2efdedad9d65a2e89f8381510ffc
Juan Sastre Montero <jsm00@iesemilidarder.com>
2026-04-23 12:35:30 +0200

    Ordenar por y product.html

--- js/app.js
+++ js/app.js
@@ -32,11 +32,11 @@ const getProducts = async () =>{
     return data
 }
 
-const renderProducts = (container, products) =>{
+const renderProducts = (container, products) =>{ //Renderizar productos de una lista en un contenedor
     container.innerHTML = ""
     products.forEach(product => {
         container.innerHTML+=`
-            <article>
+            <article class="product">
                 <div class="contenedorOverflow">
                     <img src="/img/Productos/${product.img}"/>
                 </div>
@@ -49,8 +49,8 @@ const renderProducts = (container, products) =>{
     })
 }
 
-//Mostrar productos página index
-const renderIndexProducts = ()=>{
+
+const renderIndexProducts = ()=>{ //Mostrar productos página index
     productsContainers.forEach(c=> {
         if (c.dataset.section === "novedades"){
             const newList = productsList.slice(-4)
@@ -64,7 +64,7 @@ const renderIndexProducts = ()=>{
 }
 
 //Empaginado
-const pagination = (list)=>{
+const pagination = (list)=>{ //Añadir numeros de cambio de página en función de una lista
     const btnsPags = document.querySelector(".paginas")
     btnsPags.innerHTML=""
     for (let i = 1; i <= Math.ceil(list.length / numProdPagina); i++) {
@@ -77,30 +77,30 @@ const pagination = (list)=>{
     }
 }
 
-const renderPage = (list, page)=>{
+const renderPage = (list, page)=>{ //Mostrar productos de una lista por páginas 
     let primerProdMostrado = (page - 1) * numProdPagina
     let ultimoProdMostrado = primerProdMostrado + numProdPagina
     const pagina = list.slice(primerProdMostrado, ultimoProdMostrado)
     renderProducts(productsContainers[0], pagina)
 }
 
-//Mostrar productos collections   !!!!!HACE FALTA REFACTORIZAR
-const renderCollection = (collection) => {
+
+const renderCollection = (collection) => { //Mostrar productos collections
     const colTitle = document.querySelector("#colection h1")
-    if (collection == "accesorios"){
-        colList = productsList.filter(p => p.section==collection || p.section == "new era")
-    }else{
-        colList = productsList.filter(p => p.section==collection)
-    }
-    renderPage(colList, 1)
+    collection == "accesorios" 
+        ? colList = productsList.filter(p => p.section==collection || p.section == "new era") 
+        : colList = productsList.filter(p => p.section==collection)
+    renderPage(colList, 1) 
     pagination(colList)
     
     colTitle.innerHTML = collection.toUpperCase()
     colNum.innerHTML = `${colList.length} productos`
+    renderSizes()
+    renderBrands()
 }
 
-//Filtrar lista por nombre, seccion y marca
-const buscar = (palabra) =>{
+
+const buscar = (palabra) =>{ //Filtrar lista por nombre, seccion y marca
     palabraMayus = palabra.toUpperCase()
     const searchedList = productsList.filter(p=>p.name.toUpperCase().includes(palabraMayus) || 
         p.section.toUpperCase().includes(palabraMayus) || 
@@ -108,10 +108,10 @@ const buscar = (palabra) =>{
     return searchedList
 }
 
-const renderBusqueda = ()=>{
+const renderBusqueda = ()=>{ //Mostrar resultado de búsqueda y añadir eventListener al segundo buscador
     const resultado = buscar(busqueda)
     colList = resultado
-    buscadorTag.value=busqueda
+    buscadorTag.value=busqueda //Estético
     buscador2Event()
     if(resultado.length < 1){
         const containerProducts = document.querySelector(".sectionProductos")
@@ -124,10 +124,11 @@ const renderBusqueda = ()=>{
         pagination(colList)
     }  
     colNum.innerHTML = `${colList.length} productos`
-    console.log(colList)
+    renderSizes()
+    renderBrands()
 }
-//Añadir eventlistener al input del header
-const buscadorEvent = () =>{
+
+const buscadorEvent = () =>{ //Añadir eventlistener al input del header
     buscadorTag.addEventListener("keydown", function(e){
         if(e.key === "Enter"){
             const texto = buscadorTag.value;
@@ -135,8 +136,8 @@ const buscadorEvent = () =>{
         }
     })
 }
-//Detalles y listener input del main de la pagina search
-const buscador2Event = ()=>{
+
+const buscador2Event = ()=>{ //Detalles y listener input del main de la pagina search
     if(buscadorTag2){
         buscadorTag2.value=busqueda
         buscadorTag2.addEventListener("keydown", function(e){
@@ -149,8 +150,8 @@ const buscador2Event = ()=>{
 }
 
 //Filtros 
-const renderSizes = ()=>{
-    const sizesContainer = document.getElementById("talla")
+const renderSizes = ()=>{ //Según la colección, cargar opciones de tallas
+    const sizesContainer = document.getElementById("talla") //No tiene carga dinámica porque pueden haber tallas que no estan disponibles
     sizesContainer.innerHTML = `<summary>TALLA</summary>`
     let sizes = []
     if (collection == "sneakers"){
@@ -176,7 +177,7 @@ const renderSizes = ()=>{
     )
 }
 
-const renderBrands = ()=>{
+const renderBrands = ()=>{ //Según la sección, cargar dinámicamente las marcas
     const brandsContainer = document.getElementById("marca")
     brandsContainer.innerHTML = `<summary>MARCA</summary>`
     let brands = []
@@ -193,8 +194,8 @@ const renderBrands = ()=>{
     `)
 }
 
-const visibilityFilter = (visibilidad)=>{
-    filterMenu.style.visibility = visibilidad
+const visibilityFilter = (visibilidad)=>{ //Declarada en un onclick del boton btn-filtrar
+    filterMenu.style.visibility = visibilidad  //Mostrar o ocultar menu de filtrado y bloquear o no el scroll mediante el overflow
     const body = document.querySelector("body")
     if(visibilidad=="visible"){
         body.style.overflow = "hidden"
@@ -203,7 +204,7 @@ const visibilityFilter = (visibilidad)=>{
     }
 }
 
-const toggleFilter = (type, value, elemento) => {
+const toggleFilter = (type, value, elemento) => { //Añadir/borrar elemento de filtrado a la lista de filtros
     const list = filters[type]
     const index = list.indexOf(value)
 
@@ -212,7 +213,7 @@ const toggleFilter = (type, value, elemento) => {
 }
 
 
-const applyFilter = ()=>{
+const applyFilter = ()=>{ //Aplicar filtro acumulativamente
     const minPrice = document.getElementById("minPrice")
     const maxPrice = document.getElementById("maxPrice")
 
@@ -220,41 +221,51 @@ const applyFilter = ()=>{
 
     if(filters.stock.includes("in") && !filters.stock.includes("notIn")){
         filteredList = filteredList.filter(p=>p.stock > 0)
-    }else if(filters.stock.includes("notIn") && !filters.stock.includes("in")){
-        filteredList = filteredList.filter(p=>p.stock == 0)
+    }else if(
+        filters.stock.includes("notIn") && !filters.stock.includes("in")){filteredList = filteredList.filter(p=>p.stock == 0)
     }
-    if(filters.sizes.length > 0){
-        filteredList = filteredList.filter(p=> p.size.some(s => filters.sizes.includes(s)))
-    }
-    if(filters.brands.length > 0){
-        filteredList = filteredList.filter(p=> p.brand.some(b => filters.brands.includes(b)))
-    }
+    if(filters.sizes.length > 0){filteredList = filteredList.filter(p=> p.size.some(s => filters.sizes.includes(s)))}
+    if(filters.brands.length > 0){filteredList = filteredList.filter(p=> p.brand.some(b => filters.brands.includes(b)))}
     if(minPrice.value){filteredList = filteredList.filter(p=> p.price >= parseFloat(minPrice.value))}
     if(maxPrice.value){filteredList = filteredList.filter(p=> p.price <= parseFloat(maxPrice.value))}
+    
     colNum.innerHTML = `${filteredList.length} productos`
     renderPage(filteredList, 1)
     pagination(filteredList)
-        console.log(filteredList)
+}
 
+//Ordenar por
+const sortProducts = (sortValue)=>{
+    let sortedList = []
+    filteredList.length > 0 ? sortedList=filteredList : sortedList=colList 
+
+    if(sortValue=="az"){
+        sortedList = sortedList.sort((p1, p2) => p1.name.localeCompare(p2.name))
+    }else if(sortValue=="za"){
+        sortedList = sortedList.sort((p1, p2) => p2.name.localeCompare(p1.name))
+    }else if(sortValue=="min-max"){
+        sortedList = sortedList.sort((p1, p2) => p1.price - p2.price)
+    }else if(sortValue=="max-min"){
+        sortedList = sortedList.sort((p1, p2) => p2.price - p1.price)
+    }
+    renderPage(sortedList, 1)
+    pagination(sortedList)
 }
 
 //MAIN
 const init = async () =>{
     productsList = await getProducts()
     renderIndexProducts() 
+    
 
-    if (collection) {
-        renderCollection(collection)
-        renderSizes()
-        renderBrands()
-    }
+    if (collection) {renderCollection(collection)}
 
     buscadorEvent()
-    if (busqueda){
-        renderBusqueda()
-        renderSizes()
-        renderBrands()
+    if (busqueda){renderBusqueda()}
+
+    if(busqueda || collection){
+        const sortTag = document.getElementById("ordenar")
+        sortTag.addEventListener("change",()=>sortProducts(sortTag.value))
     }
 }
-
 init()
